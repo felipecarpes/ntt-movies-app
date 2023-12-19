af9359218a4b55d80c28d29c5255e5cc532ee684
felipecarpes <carpesz94@gmail.com>
2023-12-19 17:15:30 -0300

    creating movie details service

---
+++ src/app/services/movie-details.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './enviroment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieDetailsService {
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getMovieDetails(movieId: string): Observable<any> {
+    const params = new HttpParams()
+      .set('apikey', environment.apiKey)
+      .set('i', movieId);
+
+    return this.http.get<any>(environment.apiUrl, { params });
+  }
+}
