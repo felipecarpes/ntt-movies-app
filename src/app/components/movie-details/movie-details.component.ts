7a81e69b45252dd792c58259fbe29de62eaa6a3d
felipecarpes <carpesz94@gmail.com>
2023-12-19 17:13:48 -0300

    creating movie details component

---
+++ src/app/components/movie-details/movie-details.component.ts
@@ -0,0 +1,20 @@
+// movie-details.component.ts
+import { Component, Input, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import * as MovieDetailsActions from '../../store/movie-details/movie-details.actions';
+
+@Component({
+  selector: 'app-movie-details',
+  standalone: true,
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.scss']
+})
+export class MovieDetailsComponent {
+  @Input() movie: any;
+
+  constructor(private store: Store<{}>) {}
+
+  goBack(): void {
+    this.store.dispatch(MovieDetailsActions.clearMovieDetails());
+  }
+}
