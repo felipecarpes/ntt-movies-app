630545298a7dc7100318eadb9c9718f12499fcfa
felipecarpes <carpesz94@gmail.com>
2023-12-19 17:14:34 -0300

    import ngrx prodivers and effects and configure dev tools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { importProvidersFrom, isDevMode } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
+import { appReducer, appEffects } from './store/app-state';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    provideStore(appReducer),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects(appEffects)
+  ]
 };
